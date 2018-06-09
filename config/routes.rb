01d2fee7fe12d7da42aa86d72018c37a41dab1c1
Jonathan <bklynbest718@gmail.com>
2018-06-09 05:44:28 -0400

    created and routes static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :restaurants do
     resources :menus
   end
-  root 'restaurants#index'
+  root 'static_pages#home'
+  get "/static_pages/contact", :as => "contact"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
